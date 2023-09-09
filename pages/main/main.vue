7536bcb18b4aa1f49004e2ae904a86a74f392017
intelrug <intelrug@gmail.com>
2023-09-09 13:57:26 +0300

    feat: rework events display

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="main">
     <div class="main__player">
-      <b-tile v-if="event" class="tile_padding_small tile_borders_top main__row">
-        <a v-if="isLive" href="#" class="main__announcement">
+      <b-tile
+        v-for="event of filteredEvents"
+        :key="event.startsAt"
+        class="tile_padding_small tile_borders_top main__row"
+      >
+        <a v-if="isLive && liveEvent && event.startsAt === liveEvent.startsAt" href="#" class="main__announcement">
           <span>{{ $t('announcement.live') }}</span>
-          – {{ summary }}
+          – {{ event.summary }}
         </a>
         <a v-else href="#" class="main__announcement">
-          <span>{{ date }}</span> {{ $t('announcement.at') }} <span>{{ time }}</span>
-          <i v-if="lessThan10Hours"> ({{ $t('announcement.in') }} {{ remainingTime | toRemainingTime }}) </i>
-          – {{ summary }}
+          <span>{{ event.startsAt | toFormattedDate }}</span> {{ $t('announcement.at') }}
+          <span>{{ event.startsAt | toFormattedTime }}</span>
+          <i v-if="lessThan10Hours(event.startsAt)">
+            ({{ $t('announcement.in') }} {{ remainingTime(event.startsAt) | toRemainingTime }})
+          </i>
+          – {{ event.summary }}
         </a>
       </b-tile>
-      <b-tile class="tile_padding_medium main__row" :class="[{ tile_borders_top: !event }]">
+      <b-tile class="tile_padding_medium main__row" :class="[{ tile_borders_top: isLive }]">
         <b-player />
       </b-tile>
       <b-tile class="tile_padding_small tile_borders_bottom main__row">
@@ -35,14 +42,27 @@ import BPlayerCompact from '~/components/player-compact/player-compact.vue';
 
 @Component({
   components: { BPlayerCompact, BChat, BPlayerButtons, BTile, BPlayer },
-  filters: { toRemainingTime },
+  filters: {
+    toRemainingTime,
+    toFormattedDate(timestamp: number): string {
+      return DateTime.fromMillis(timestamp).toFormat('dd.MM.yyyy');
+    },
+    toFormattedTime(timestamp: number): string {
+      return DateTime.fromMillis(timestamp).toFormat('HH:mm');
+    },
+  },
 })
 export default class MainPage extends Vue {
   get events(): CalendarEvent[] {
     return this.$accessor.player.calendarEventsState || [];
   }
 
-  get event(): CalendarEvent | null {
+  get filteredEvents(): CalendarEvent[] {
+    return this.events
+      .filter((event) => event.endsAt >= this.$accessor.now || event.startsAt === this.liveEvent?.startsAt)
+      .sort((a, b) => a.startsAt - b.startsAt);
+  }
+  get liveEvent(): CalendarEvent | null {
     if (!this.isLive) {
       return this.events.find((e) => e.endsAt >= Date.now()) || null;
     }
@@ -59,30 +79,18 @@ export default class MainPage extends Vue {
     return this.events[index];
   }
 
-  get lessThan10Hours(): boolean {
-    return this.remainingTime < 10 * 60 * 60 && this.remainingTime !== 0;
-  }
-
-  get date(): string {
-    return DateTime.fromMillis(this.event?.startsAt || 0).toFormat('dd.MM.yyyy');
-  }
-
-  get time(): string {
-    return DateTime.fromMillis(this.event?.startsAt || 0).toFormat('HH:mm');
-  }
-
-  get remainingTime(): number {
-    let time = (this.event?.startsAt || 0) - this.$accessor.now;
-    if (time < 0) time = 0;
-    return time / 1000;
-  }
-
   get isLive() {
     return this.$accessor.player.playingData.live.isLive;
   }
 
-  get summary(): string {
-    return this.event?.summary || '';
+  lessThan10Hours(timestamp: number): boolean {
+    return this.remainingTime(timestamp) < 10 * 60 * 60 && this.remainingTime(timestamp) !== 0;
+  }
+
+  remainingTime(timestamp: number): number {
+    let time = timestamp - this.$accessor.now;
+    if (time < 0) time = 0;
+    return time / 1000;
   }
 }
 </script>
